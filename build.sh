ed6f228a3abd06589cf6ee3e62bab60577b177bc
Yaroslav Yamshchikov <y.yamshchiko@samsung.com>
2019-08-05 21:01:25 +0300

    build with test of the queue

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 mv exe old_exe
 g++ -c stack.cpp -std=c++17
 g++ -c queue.cpp -std=c++17
+g++ -c queue_test.cpp -std=c++17
 g++ -c datastructs.cpp -std=c++17
-g++ -o exe stack.o queue.o datastructs.o -std=c++17
+g++ -o exe stack.o queue.o queue_test.o datastructs.o -std=c++17
 
--- datastructs.cpp
+++ datastructs.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <random>
-#include "queue.h"
-#include "stack.h"
 #include <functional>
+#include "queue.h"
+#include "queue_test.h"
+#include "stack.h"
 
 int STORAGE[256000];
 
 void stack_test(void);
-int queue_test(unsigned int seed, unsigned int n);
 
 int main(int argc, char *argv[])
 {
@@ -58,90 +58,3 @@ void stack_test(void)
 	std::cout<<std::endl;
 }
 
-int queue_test(unsigned int seed, unsigned int n)
-{
-	if (n == 0)
-	{
-		return 0;
-	}
-
-	std::default_random_engine generator(seed);
-	std::uniform_int_distribution<int> distribution(1, 999999);
-	auto dice = std::bind(distribution, generator);
-
-	queue q;
-	queue_init(&q, STORAGE, 5);
-
-	std::vector<int> in;
-	std::vector<int> out;
-
-	int test_set_size = dice();
-	std::cout<<test_set_size<<std::endl;
-
-	for (size_t i = 0; i < test_set_size; i++)
-	{
-		int number = dice();
-		in.push_back(number);
-		int elem = -1;
-		if (queue_isfull(&q))
-		{
-			if (queue_pop(&q, &elem) == 0)
-			{
-				out.push_back(elem);
-			}
-			else
-			{
-				std::cerr<<"cant't pop from full queue, inconsistence"<<std::endl;
-				return 1;
-			}
-		}
-		elem = -1;
-		queue_push(&q, number);
-		int dist[] = {1,1,1,1,0,0,5,2,0,0,0};
-		size_t sd = sizeof(dist)/sizeof(dist[0]);
-		int f = dist[dice()%sd];
-		for (size_t j = 0; j < f; ++j)
-		{
-			if (queue_pop(&q, &elem) == 0)
-			{
-				out.push_back(elem);
-			}
-		}
-	}
-	size_t q_size = queue_size(&q);
-	for (size_t i = 0; i < q_size; ++i)
-	{
-		int elem = -1;
-		if (queue_pop(&q, &elem) == 0)
-		{
-			out.push_back(elem);
-		}
-		else
-		{
-			std::cerr<<"queue size is inconsistent"<<std::endl;
-			return 1;
-		}
-	}
-	if (in.size() != out.size())
-	{
-		std::cerr<<"in and out sets are of different size"<<std::endl;
-		std::cerr<<"in.size = "<<in.size()<<"; out.size = "<<out.size()<<std::endl;
-		return 1;
-	}
-	size_t in_size = in.size();
-	for (size_t i = 0; i < in_size; ++i)
-	{
-		if (in[i] != out[i])
-		{
-			std::cerr<<"in and out queues differ at position "<<i<<", seed = "<<seed<<std::endl;
-			return 1;
-		}
-		if (out[i] == -1)
-		{
-			std::cerr<<"dumb value encountered, queue is corrupted"<<std::endl;
-			return 0;
-		}
-	}
-	std::cerr<<"queue passed the test"<<std::endl;
-	return queue_test(seed-1, n-1);
-}
